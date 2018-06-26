69f19988b49fd9d4328fadce3213c5298fe7a9dd
Felixvl <digitaleservice@gmail.com>
2018-06-26 13:19:13 +0200

    backend is setup

---
+++ backend/boatrace/src/main/java/com/leiden/boatrace/BoatraceApplication.java
@@ -0,0 +1,12 @@
+package com.leiden.boatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatraceApplication.class, args);
+	}
+}
