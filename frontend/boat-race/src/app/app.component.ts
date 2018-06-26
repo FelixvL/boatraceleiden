cc0322d42168c0486ab44912f698bf05f6432b60
Felixvl <digitaleservice@gmail.com>
2018-06-26 10:23:47 +0200

    The variables are working

--- frontend/boat-race/src/app/app.component.ts
+++ frontend/boat-race/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appp';
+  title = 'boatrace';
+  nameBoat1 = 'Titanic';
+  nameBoat2 = 'Bismark';
 }
